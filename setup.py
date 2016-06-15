deb97ca0f779e726987df88dba1b93dc32ed42a0
Rob Hague <rob.hague@cydar.co.uk>
2016-06-15 21:29:40 +0100

    Added `setup.py`.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='quicli',
+      version='0.1',
+      description='Effortless CLI',
+      author='Rob Hague',
+      author_email='rob.hague@cydar.co.uk',
+      url='https://github.com/CydarLtd/quicli',
+      py_modules=['quicli'],
+      license='MIT',
+      )
